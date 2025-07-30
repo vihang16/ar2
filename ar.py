d0b20dbcf7e2d788162211f37f8ab9c92fcfc846
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-07-30 14:58:59 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -88,9 +88,8 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# Display dubai.png above the title
-#dubai_image_url = supabase.storage.from_("ar").get_public_url("dubai.png")
-#st.image(dubai_image_url, height=100, caption="", use_column_width=False, cls="header-image")
+# Display dubai.png from local GitHub repository
+st.image("dubai.png", height=100, caption="", use_column_width=False, cls="header-image")
 
 
 st.title("AR Tennis Group 🎾")
