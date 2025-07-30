9eb218e742166a0481532bc9e69f560590af7c9c
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-07-30 15:04:38 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -89,7 +89,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Display dubai.png from local GitHub repository
-st.image("dubai.png", width=800, caption="", use_column_width=False, cls="header-image")
+st.markdown('<img src="dubai.png" class="header-image">', unsafe_allow_html=True)
 
 
 st.title("AR Tennis Group 🎾")
