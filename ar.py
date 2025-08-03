71a9a61f3ef89fd708b33071fd94bb9265168997
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-08-03 20:29:15 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -913,7 +913,7 @@ if not matches.empty:
     )
 
 st.markdown("""
-<div style='background-color: #161e80; padding: 1rem; border-left: 5px solid #fff500; border-radius: 0.5rem; color: white;'>
+<div style='background-color: #123d22; padding: 1rem; border-left: 5px solid #fff500; border-radius: 0.5rem; color: white;'>
 Built with ❤️ using <a href='https://streamlit.io/' style='color: #ccff00;'>Streamlit</a> — free and open source.
 <a href='https://devs-scripts.streamlit.app/' style='color: #ccff00;'>Other Scripts by dev</a> on Streamlit.
 </div>
