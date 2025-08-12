0ffe561ee0b47fe080689df564d3c33a78c88ad3
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-08-12 11:41:16 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -2209,7 +2209,7 @@ with col_player_backup:
         st.info("No player data available to download.")
 st.markdown("""
 <div style='background-color: #0d5384; padding: 1rem; border-left: 5px solid #fff500; border-radius: 0.5rem; color: white;'>
-Built with ❤️ using <a href='https://streamlit.io/' style='color: #ccff00;'>Streamlit</a> — free and open source.
+Built with :heart: using <a href='https://streamlit.io/' style='color: #ccff00;'>Streamlit</a> — free and open source.
 <a href='https://devs-scripts.streamlit.app/' style='color: #ccff00;'>Other Scripts by dev</a> on Streamlit.
 </div>
 """, unsafe_allow_html=True)
